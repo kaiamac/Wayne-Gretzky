8ebd97c7a0088d594e1bcfd06bf1f0dcccd48991
kamac8665 <kamac8665@7HMN6Y1.Student.UGDSB.ED>
2018-09-27 10:05:09 -0400

    This is Wayne Gretzky

---
+++ src/wayne/gretzky/WayneGretzky.java
@@ -0,0 +1,25 @@
+/*
+ * Kaia Mac
+ * This program will show Wayne Gretzky and his hockey number
+ * September 18th, 2018
+ */
+
+package wayne.gretzky;
+
+/**
+ *
+ * @author kamac8665
+ */
+public class WayneGretzky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Wayne Gretzky's");
+        double number = 99;
+        System.out.print(" Hockey number is " + number);
+        
+    }
+    
+}
